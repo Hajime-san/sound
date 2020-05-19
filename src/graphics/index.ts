f3fd917fe6cf7ea74b646903f1432555407e051d
Hajime-san <utd.c.r.d.s.a@gmail.com>
2020-05-19 22:10:13 +0900

    add time unform variable

--- src/graphics/index.ts
+++ src/graphics/index.ts
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 // import { OrbitControls } from 'three/examples/js/controls/OrbitControls';
 import { GPUComputationRenderer, GPUComputationRendererVariable } from 'gpucomputationrender-three';
 
-const WIDTH = 500;
+const WIDTH = 256;
 const PARTICLES = WIDTH * WIDTH;
 
 let renderer: any,
@@ -11,7 +11,10 @@ let renderer: any,
     scene: any,
     camera: any,
     container: any,
-    controls: any;
+    controls: any,
+    rot: number,
+    time: number,
+    delta: any;
 
 let gpuCompute: GPUComputationRenderer;
 
@@ -31,6 +34,9 @@ export const init = () => {
   camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 5, 15000 );
   camera.position.y = 0;
   camera.position.z = 150;
+  rot = 0;
+  time = 0;
+  delta = new THREE.Clock();
   scene = new THREE.Scene();
   renderer = new THREE.WebGLRenderer();
   renderer.setClearColor( 0x000000 );
@@ -78,6 +84,7 @@ export function initComputeRenderer() {
   velocityUniforms.ambient = {
     value: [1.0,  0.0,  0.0]
   };
+  velocityUniforms.time = { value: 0.0 };
 
   const error = gpuCompute.init();
 
@@ -192,6 +199,15 @@ export function animate() {
 
   particleUniforms.texturePosition.value = gpuCompute.getCurrentRenderTarget( positionVariable ).texture;
   particleUniforms.textureVelocity.value = gpuCompute.getCurrentRenderTarget( velocityVariable ).texture;
+  time += delta.getDelta();
+  velocityUniforms.time.value = time;
+
+  rot += 0.5;
+  const radian = (rot * Math.PI) / 180;
+  camera.position.x = 150 * Math.sin(radian);
+  camera.position.z = 150 * Math.cos(radian);
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+
   renderer.render( scene, camera );
 }
 
@@ -283,8 +299,8 @@ function computeShaderVelocity() {
     #include <common>
 
     uniform float volume;
+    uniform float time;
 
-    //
     // Description : Array and textureless GLSL 2D/3D/4D simplex
     //               noise functions.
     //      Author : Ian McEwan, Ashima Arts.
@@ -296,15 +312,17 @@ function computeShaderVelocity() {
     //               https://github.com/stegu/webgl-noise
     //
 
-    vec3 mod289(vec3 x) {
-      return x - floor(x * (1.0 / 289.0)) * 289.0;
-    }
-
     vec4 mod289(vec4 x) {
-      return x - floor(x * (1.0 / 289.0)) * 289.0;
-    }
+      return x - floor(x * (1.0 / 289.0)) * 289.0; }
+
+    float mod289(float x) {
+      return x - floor(x * (1.0 / 289.0)) * 289.0; }
 
     vec4 permute(vec4 x) {
+        return mod289(((x*34.0)+1.0)*x);
+    }
+
+    float permute(float x) {
         return mod289(((x*34.0)+1.0)*x);
     }
 
@@ -313,86 +331,115 @@ function computeShaderVelocity() {
       return 1.79284291400159 - 0.85373472095314 * r;
     }
 
-    float snoise(vec3 v)
+    float taylorInvSqrt(float r)
+    {
+      return 1.79284291400159 - 0.85373472095314 * r;
+    }
+
+    vec4 grad4(float j, vec4 ip)
       {
-      const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
-      const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
+      const vec4 ones = vec4(1.0, 1.0, 1.0, -1.0);
+      vec4 p,s;
+
+      p.xyz = floor( fract (vec3(j) * ip.xyz) * 7.0) * ip.z - 1.0;
+      p.w = 1.5 - dot(abs(p.xyz), ones.xyz);
+      s = vec4(lessThan(p, vec4(0.0)));
+      p.xyz = p.xyz + (s.xyz*2.0 - 1.0) * s.www;
+
+      return p;
+      }
+
+    // (sqrt(5) - 1)/4 = F4, used once below
+    #define F4 0.309016994374947451
+
+    float snoise(vec4 v)
+      {
+      const vec4  C = vec4( 0.138196601125011,  // (5 - sqrt(5))/20  G4
+                            0.276393202250021,  // 2 * G4
+                            0.414589803375032,  // 3 * G4
+                          -0.447213595499958); // -1 + 4 * G4
 
     // First corner
-      vec3 i  = floor(v + dot(v, C.yyy) );
-      vec3 x0 =   v - i + dot(i, C.xxx) ;
+      vec4 i  = floor(v + dot(v, vec4(F4)) );
+      vec4 x0 = v -   i + dot(i, C.xxxx);
 
     // Other corners
-      vec3 g = step(x0.yzx, x0.xyz);
-      vec3 l = 1.0 - g;
-      vec3 i1 = min( g.xyz, l.zxy );
-      vec3 i2 = max( g.xyz, l.zxy );
 
-      //   x0 = x0 - 0.0 + 0.0 * C.xxx;
-      //   x1 = x0 - i1  + 1.0 * C.xxx;
-      //   x2 = x0 - i2  + 2.0 * C.xxx;
-      //   x3 = x0 - 1.0 + 3.0 * C.xxx;
-      vec3 x1 = x0 - i1 + C.xxx;
-      vec3 x2 = x0 - i2 + C.yyy; // 2.0*C.x = 1/3 = C.y
-      vec3 x3 = x0 - D.yyy;      // -1.0+3.0*C.x = -0.5 = -D.y
+    // Rank sorting originally contributed by Bill Licea-Kane, AMD (formerly ATI)
+      vec4 i0;
+      vec3 isX = step( x0.yzw, x0.xxx );
+      vec3 isYZ = step( x0.zww, x0.yyz );
+    //  i0.x = dot( isX, vec3( 1.0 ) );
+      i0.x = isX.x + isX.y + isX.z;
+      i0.yzw = 1.0 - isX;
+    //  i0.y += dot( isYZ.xy, vec2( 1.0 ) );
+      i0.y += isYZ.x + isYZ.y;
+      i0.zw += 1.0 - isYZ.xy;
+      i0.z += isYZ.z;
+      i0.w += 1.0 - isYZ.z;
+
+      // i0 now contains the unique values 0,1,2,3 in each channel
+      vec4 i3 = clamp( i0, 0.0, 1.0 );
+      vec4 i2 = clamp( i0-1.0, 0.0, 1.0 );
+      vec4 i1 = clamp( i0-2.0, 0.0, 1.0 );
+
+      //  x0 = x0 - 0.0 + 0.0 * C.xxxx
+      //  x1 = x0 - i1  + 1.0 * C.xxxx
+      //  x2 = x0 - i2  + 2.0 * C.xxxx
+      //  x3 = x0 - i3  + 3.0 * C.xxxx
+      //  x4 = x0 - 1.0 + 4.0 * C.xxxx
+      vec4 x1 = x0 - i1 + C.xxxx;
+      vec4 x2 = x0 - i2 + C.yyyy;
+      vec4 x3 = x0 - i3 + C.zzzz;
+      vec4 x4 = x0 + C.wwww;
 
     // Permutations
       i = mod289(i);
-      vec4 p = permute( permute( permute(
-                i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
-              + i.y + vec4(0.0, i1.y, i2.y, 1.0 ))
-              + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
+      float j0 = permute( permute( permute( permute(i.w) + i.z) + i.y) + i.x);
+      vec4 j1 = permute( permute( permute( permute (
+                i.w + vec4(i1.w, i2.w, i3.w, 1.0 ))
+              + i.z + vec4(i1.z, i2.z, i3.z, 1.0 ))
+              + i.y + vec4(i1.y, i2.y, i3.y, 1.0 ))
+              + i.x + vec4(i1.x, i2.x, i3.x, 1.0 ));
 
-    // Gradients: 7x7 points over a square, mapped onto an octahedron.
-    // The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
-      float n_ = 0.142857142857; // 1.0/7.0
-      vec3  ns = n_ * D.wyz - D.xzx;
+    // Gradients: 7x7x6 points over a cube, mapped onto a 4-cross polytope
+    // 7*7*6 = 294, which is close to the ring size 17*17 = 289.
+      vec4 ip = vec4(1.0/294.0, 1.0/49.0, 1.0/7.0, 0.0) ;
 
-      vec4 j = p - 49.0 * floor(p * ns.z * ns.z);  //  mod(p,7*7)
+      vec4 p0 = grad4(j0,   ip);
+      vec4 p1 = grad4(j1.x, ip);
+      vec4 p2 = grad4(j1.y, ip);
+      vec4 p3 = grad4(j1.z, ip);
+      vec4 p4 = grad4(j1.w, ip);
 
-      vec4 x_ = floor(j * ns.z);
-      vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
-
-      vec4 x = x_ *ns.x + ns.yyyy;
-      vec4 y = y_ *ns.x + ns.yyyy;
-      vec4 h = 1.0 - abs(x) - abs(y);
-
-      vec4 b0 = vec4( x.xy, y.xy );
-      vec4 b1 = vec4( x.zw, y.zw );
-
-      //vec4 s0 = vec4(lessThan(b0,0.0))*2.0 - 1.0;
-      //vec4 s1 = vec4(lessThan(b1,0.0))*2.0 - 1.0;
-      vec4 s0 = floor(b0)*2.0 + 1.0;
-      vec4 s1 = floor(b1)*2.0 + 1.0;
-      vec4 sh = -step(h, vec4(0.0));
-
-      vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
-      vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
-
-      vec3 p0 = vec3(a0.xy,h.x);
-      vec3 p1 = vec3(a0.zw,h.y);
-      vec3 p2 = vec3(a1.xy,h.z);
-      vec3 p3 = vec3(a1.zw,h.w);
-
-    //Normalise gradients
+    // Normalise gradients
       vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
       p0 *= norm.x;
       p1 *= norm.y;
       p2 *= norm.z;
       p3 *= norm.w;
+      p4 *= taylorInvSqrt(dot(p4,p4));
 
-    // Mix final noise value
-      vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
-      m = m * m;
-      return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1),
-                                    dot(p2,x2), dot(p3,x3) ) );
+    // Mix contributions from the five corners
+    vec3 m0 = max(0.6 - vec3(dot(x0,x0), dot(x1,x1), dot(x2,x2)), 0.0);
+    vec2 m1 = max(0.6 - vec2(dot(x3,x3), dot(x4,x4)            ), 0.0);
+    m0 = m0 * m0;
+    m1 = m1 * m1;
+    return 49.0 * ( dot(m0*m0, vec3( dot( p0, x0 ), dot( p1, x1 ), dot( p2, x2 )))
+                + dot(m1*m1, vec2( dot( p3, x3 ), dot( p4, x4 ) ) ) ) ;
+
+    }
+
+    float RestirictedRandom(float min, float max, vec2 p) {
+      return min + floor(rand(p) * (max - min + 1.0));
     }
 
     void main() {
       vec2 uv = gl_FragCoord.xy / resolution.xy;
-      float idParticle = uv.y * resolution.x + uv.x;
       vec4 tmpPos = texture2D( texturePosition, uv );
       vec3 pos = tmpPos.xyz;
+      float index = tmpPos.w;
+
       vec4 tmpVel = texture2D( textureVelocity, uv );
       vec3 vel = tmpVel.xyz;
 
@@ -401,10 +448,17 @@ function computeShaderVelocity() {
       float phi = angleData.x;
       float theta = angleData.y;
 
+      float r = rand(gl_FragCoord.xy);
+
+      float random = RestirictedRandom(volume - 10.0 * r, volume + 10.0 * r, gl_FragCoord.xy);
+
       // update sphere with volume as radius
-      vel.x = volume * cos(theta) * sin(phi);
-      vel.y = volume * sin(theta);
-      vel.z = volume * cos(theta) * cos(phi);
+      vec3 newPos = vec3(0.0);
+      newPos.x = (volume * (2.0 * snoise( vec4( 0.1 * newPos.xyz, 7.225 + 0.5 * time ) ))) * cos(theta) * sin(phi);
+      newPos.y = (volume * (2.0 * snoise( vec4( 0.1 * newPos.xyz, 1.035 + 0.5 * time ) )))  * sin(theta);
+      newPos.z = (volume * (2.0 * snoise( vec4( 0.1 * newPos.xyz, 3.669 + 0.5 * time ) )))  * cos(theta) * cos(phi);
+
+      vel = newPos;
 
       gl_FragColor = vec4( vel, 0.0 );
     }
